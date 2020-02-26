dc9dda82f2d38c76450767d3e824c6012b353dd2
ngo <shota.nagao@exmed.io>
2020-02-26 16:26:22 +0900

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "./comps/MyLayout";
+
+const Page = () => <p>This is the about page</p>;
+
+export default function About() {
+    return (
+        <Layout>
+            <Page />
+        </Layout>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "./comps/MyLayout";
+
+export default function Index() {
+    return (
+        <Layout>
+            <h1>Top Level</h1>
+        </Layout>
+    )
+}
